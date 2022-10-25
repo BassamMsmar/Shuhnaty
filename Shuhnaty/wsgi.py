bec3b98fa1833d75c68f91155b9931bac70bfd1b
BassamMsmar <bassammsmar@gmail.com>
2022-10-25 14:51:08 +0000

    add setting to manage

--- Shuhnaty/wsgi.py
+++ Shuhnaty/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shuhnaty.settings.local_settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shuhnaty.settings.production_settings')
 
 application = get_wsgi_application()
