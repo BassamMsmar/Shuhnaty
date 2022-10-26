83953a6f9fd4b5ec27469a68f1c47b0463cddbe8
Shuhnaty <shuhnaty@gmail.com>
2022-10-26 07:07:26 +0300

    fiexed model

--- shipments/models.py
+++ shipments/models.py
@@ -13,7 +13,7 @@ from drivers.models import DriverModel
 class ShipmentModel(models.Model):
     Shipment_id = models.BigAutoField(primary_key=True)
     driver = models.ForeignKey(DriverModel, on_delete=models.CASCADE)
-    company = models.ForeignKey(CompanyModel, on_delete=models.CASCADE)
+    company = models.ForeignKey(CompanyModel, on_delete=models.CASCADE, null=True)
     destination =  models.CharField(max_length=225,  help_text=' الوجهة')
     source =  models.CharField(max_length=225,  default='' , help_text=' المصدر')
     amount =  models.CharField(max_length=225,  help_text=' الاجرة')
