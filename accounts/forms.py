9dc265b8e9b3998e079b4c4fffd9f2d45baff0ec
Shuhnaty <shuhnaty@gmail.com>
2022-11-27 14:46:14 +0300

    add and fix catch

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,9 +9,10 @@ User = get_user_model()
 class SingUpForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=False, help_text='احرف انجليزية فقط ولا يحتوي على مسافات')
     number_phone = forms.CharField(max_length=16, required=False, help_text='966 5xxx xxxx xx')
-    # first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    # last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
     # email = forms.EmailField(max_length=254, required=False, help_text='Required! Inform a valid email address')
+
 
     class Meta:
         model = User
